f8343c12464303a0c2493f22ea5e4e5f9246c4e2
Monika <monicljamonic@gmail.com>
2021-01-12 19:44:45 +0200

    Changes in front HomePageController

--- src/main/java/com/example/demo/onlineshop/front/HomePageController.java
+++ src/main/java/com/example/demo/onlineshop/front/HomePageController.java
@@ -10,4 +10,52 @@ public class HomePageController {
     public String home() {
         return "shop/index";
     }
+
+    @GetMapping("/about")
+    public String about() {
+        return "shop/about";
+    }
+
+    @GetMapping("/cart")
+    public String cart() {
+        return "shop/cart";
+    }
+
+    @GetMapping("/checkout")
+    public String checkout() {
+        return "shop/checkout";
+    }
+
+    @GetMapping("/contactus")
+    public String contactUs() {
+        return "shop/contact-us";
+    }
+
+    @GetMapping("/myaccount")
+    public String myAccount() {
+        return "shop/my-account";
+    }
+
+    @GetMapping("/service")
+    public String service() {
+        return "shop/service";
+    }
+
+    @GetMapping("/shop")
+    public String shop() {
+        return "shop/shop";
+    }
+
+    @GetMapping("/shopdetails")
+    public String shopDetails() {
+        return "shop/shop-detail";
+    }
 }
+
+
+
+
+
+
+
+
