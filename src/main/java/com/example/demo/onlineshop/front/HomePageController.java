bdabef1a2565d3818eaa7bb077a767559319b07b
LauraDobele <LauraDobele@users.noreply.github.com>
2021-01-14 06:40:31 +0200

    Latest changes in HomePageController and user html

--- src/main/java/com/example/demo/onlineshop/front/HomePageController.java
+++ src/main/java/com/example/demo/onlineshop/front/HomePageController.java
@@ -1,13 +1,39 @@
 package com.example.demo.onlineshop.front;
 
+import com.example.demo.onlineshop.categories.Categories;
+import com.example.demo.onlineshop.categories.CategoriesRepository;
+import com.example.demo.onlineshop.products.ProductRequest;
+import com.example.demo.onlineshop.products.ProductsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
 
 @Controller
+@RequestMapping
 public class HomePageController {
+    private final ProductsRepository repository;
+    private final CategoriesRepository categoriesRepository;
 
-    @GetMapping("/")
-    public String home() {
+    @Autowired
+    public HomePageController(CategoriesRepository categoriesRepository, ProductsRepository repository) {
+        this.categoriesRepository = categoriesRepository;
+        this.repository = repository;
+    }
+
+    @GetMapping(path = {"/"})
+    public String home(@PathVariable(required = false) Long productId, Model model) {
+        List<Categories> allCategories = categoriesRepository.findAll();
+        model.addAttribute("allCategories", allCategories);
+        List<ProductRequest> allProducts = repository.findAll();
+        model.addAttribute("allProducts", allProducts);
+//        Products product = repository.findOne(productId);
+//        model.addAttribute("product", product);
         return "shop/index";
     }
 
@@ -36,12 +62,26 @@ public class HomePageController {
         return "shop/my-account";
     }
 
-    @GetMapping("/products")
-    public String products() {
-        return "shop/products";
+    @GetMapping(path = {"/products", "/products/{categoryId}"})
+    public String products(@PathVariable(required = false) Long categoryId, Model model) {
+        if (categoryId == null) {
+            List<Categories> allCategories = categoriesRepository.findAll();
+            model.addAttribute("allCategories", allCategories);
+            List<ProductRequest> allProducts = repository.findAll();
+            model.addAttribute("allProducts", allProducts);
+            return "shop/products";
+        } else {
+            List<Categories> allCategories = categoriesRepository.findAll();
+            model.addAttribute("allCategories", allCategories);
+            Categories category = categoriesRepository.findOne(categoryId);
+            model.addAttribute("category", category);
+            List<ProductRequest> allProducts = repository.findAll();
+            model.addAttribute("allProducts", allProducts);
+            return "shop/products";
+        }
     }
 
-    @GetMapping("/productdetails")
+    @GetMapping("/product-details")
     public String productDetails() {
         return "shop/product-details";
     }
