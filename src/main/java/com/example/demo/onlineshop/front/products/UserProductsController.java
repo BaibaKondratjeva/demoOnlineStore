42fadf70242d9e55f2c9eefe295130a101fda4a7
LauraDobele <LauraDobele@users.noreply.github.com>
2021-01-21 01:03:58 +0200

    some Lauras change.... don't know why, don't know where. Just want to solve this /products/productId problem

--- src/main/java/com/example/demo/onlineshop/front/products/UserProductsController.java
+++ src/main/java/com/example/demo/onlineshop/front/products/UserProductsController.java
@@ -1,23 +1,17 @@
 package com.example.demo.onlineshop.front.products;
 
-import com.example.demo.cookies.CookieUtils;
 import com.example.demo.onlineshop.categories.Categories;
 import com.example.demo.onlineshop.categories.CategoriesRepository;
-import com.example.demo.onlineshop.orders.Orders;
 import com.example.demo.onlineshop.orders.OrdersRepository;
 import com.example.demo.onlineshop.products.ProductRequest;
 import com.example.demo.onlineshop.products.ProductsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletResponse;
 import java.util.List;
-import java.util.UUID;
-
-import static com.example.demo.cookies.Cookies.USER_ID_COOKIE_NAME;
 
 @Controller
 public class UserProductsController {
@@ -46,7 +40,7 @@ public class UserProductsController {
         return "shop/product-details";
     }*/
 
-    @GetMapping(path = {"/products", "/products/{categoryId}"})
+    @GetMapping(path = {"/products", "/categories/{categoryId}"})
     public String products(@PathVariable(required = false) Long categoryId, Model model) {
         if (categoryId == null) {
             List<Categories> allCategories = categoriesRepository.findAll();
@@ -65,48 +59,50 @@ public class UserProductsController {
         }
     }
 
-    @GetMapping("/product-details")
-    public String productDetails() {
+    @GetMapping("/products/{id}")
+    public String productDetails(@PathVariable Long id, Model model) {
+        ProductRequest product = productsRepository.findOne(id);
+        model.addAttribute("product", product);
         return "shop/product-details";
     }
 
 
-    @PostMapping("/product/{productId}")
-    public String addProductToCart(@PathVariable Long productId,
-                                   //We can use this annotation before the parameter to tell Spring to
-                                   //set the value of this parameter to the value of cookie (if user has a cookie)
-                                   @CookieValue(name = USER_ID_COOKIE_NAME, required = false) String userId,
-                                   AddProductToCartForm form,
-                                   HttpServletResponse response,
-                                   Model model) {
-
-        ProductRequest product = productsRepository.findOne(productId);
-
-        if (userId != null){
-            userId = UUID.randomUUID().toString();
-            addShoppingCartCookieToResponse(response, userId);
-            Orders order = new Orders();
-            ordersRepository.insert(order);
-        }
-
-
-
-
-        System.out.println("add to cart product: " + productId + " and quantity " + form);
-        System.out.println("Cart cookie ID: " + userId);
-
-        model.addAttribute("productForm", new AddProductToCartForm());
-        model.addAttribute("isSuccess", true);
-
-        //String userIdentifier = UUID.randomUUID().toString(); // example how we can create unique identifier
-
-        //Example of how we can generate unique ID and set cookie for user
-
-        return "shop/product-details";
-    }
-
-    private void addShoppingCartCookieToResponse(HttpServletResponse response, String cookieValue) {
-        Cookie cookie = CookieUtils.createCookie(USER_ID_COOKIE_NAME, cookieValue);
-        response.addCookie(cookie);
-    }
+//    @PostMapping("/product/{productId}")
+//    public String addProductToCart(@PathVariable Long productId,
+//                                   //We can use this annotation before the parameter to tell Spring to
+//                                   //set the value of this parameter to the value of cookie (if user has a cookie)
+//                                   @CookieValue(name = USER_ID_COOKIE_NAME, required = false) String userId,
+//                                   AddProductToCartForm form,
+//                                   HttpServletResponse response,
+//                                   Model model) {
+//
+//        ProductRequest product = productsRepository.findOne(productId);
+//
+//        if (userId != null){
+//            userId = UUID.randomUUID().toString();
+//            addShoppingCartCookieToResponse(response, userId);
+//            Orders order = new Orders();
+//            ordersRepository.insert(order);
+//        }
+//
+//
+//
+//
+//        System.out.println("add to cart product: " + productId + " and quantity " + form);
+//        System.out.println("Cart cookie ID: " + userId);
+//
+//        model.addAttribute("productForm", new AddProductToCartForm());
+//        model.addAttribute("isSuccess", true);
+//
+//        //String userIdentifier = UUID.randomUUID().toString(); // example how we can create unique identifier
+//
+//        //Example of how we can generate unique ID and set cookie for user
+//
+//        return "shop/product-details";
+//    }
+//
+//    private void addShoppingCartCookieToResponse(HttpServletResponse response, String cookieValue) {
+//        Cookie cookie = CookieUtils.createCookie(USER_ID_COOKIE_NAME, cookieValue);
+//        response.addCookie(cookie);
+//    }
 }
