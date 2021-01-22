f675474e93539d0ec83bb03a99774011d8025055
AnnaTimofejeva <timofejevaaa@gmail.com>
2021-01-22 21:43:48 +0200

    Checkout

--- src/main/java/com/example/demo/onlineshop/front/checkout/CheckOutRepository.java
+++ src/main/java/com/example/demo/onlineshop/front/checkout/CheckOutRepository.java
@@ -1,12 +1,17 @@
 package com.example.demo.onlineshop.front.checkout;
 
 import com.example.demo.onlineshop.Customer;
-import com.example.demo.onlineshop.Orders;
+import com.example.demo.onlineshop.orders.Orders;
 
 public interface CheckOutRepository {
 
-    CheckOutForm createCustomer (CheckOutForm form);
+    Customer createCustomer (CheckOutForm form);
 
     Orders createOrder (Long customerId, Orders order);
+
+//    void checkout(CheckOutForm form, String userId);
+
+    void checkout (CheckOutForm form, Long userCookieId);
+
 
 }
